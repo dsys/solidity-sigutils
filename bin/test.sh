9c9c79d1e9b9b7fdb32d34a5c8994dadcf9f7620
Alex Kern <alex@kern.io>
2018-05-28 22:23:55 -0700

    Tweak CI

--- bin/test.sh
+++ bin/test.sh
@@ -70,5 +70,6 @@ if [ "$SOLIDITY_COVERAGE" = true ]; then
     cat coverage/lcov.info | node_modules/.bin/coveralls
   fi
 else
-  node_modules/.bin/truffle test "$@"
+  node_modules/.bin/truffle migrate --compile-all --network development
+  node_modules/.bin/truffle test --network development "$@"
 fi
