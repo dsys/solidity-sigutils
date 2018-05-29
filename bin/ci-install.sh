3324391c0c58dae6791b9bb9b2da62f6e45394fd
Alex Kern <alex@kern.io>
2018-05-28 18:36:43 -0700

    Add travis

---
+++ bin/ci-install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yarn add -g ganache-cli truffle
---
+++ bin/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+ganache-cli --gasLimit 10000000 2> /dev/null 1> /dev/null &
+sleep 5 # to make sure ganache-cli is up and running before compiling
+rm -rf build
+yarn compile
+yarn migrate
+yarn test
+kill -9 $(lsof -t -i:8545)
